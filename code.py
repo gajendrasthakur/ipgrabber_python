7c66507cfbaa64a7d295dc20b07645a8d43e9ec3
GAJENDRA SINGH THAKUR <71806431+gajendra1405@users.noreply.github.com>
2020-12-26 08:42:39 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,8 @@
+import socket as s 
+my_hostname = s.gethostname()
+print('Your Hostname is: ' + my_hostname)
+my_ip = s.gethostbyname(my_hostname) 
+print('Your Ip Address is: ' + my_ip)
+host = 'mskblogs.in'
+ip = s.gethostbyname(host)
+print('The IP Address of ' + host + ' is: '  + ip)
