7cd7862d690936ee92c37f5d8f45dce695e4550a
GAJENDRA SINGH THAKUR <71806431+gajendra1405@users.noreply.github.com>
2021-01-01 10:09:52 +0530

    Code Updated

--- code.py
+++ code.py
@@ -3,6 +3,6 @@ my_hostname = s.gethostname()
 print('Your Hostname is: ' + my_hostname)
 my_ip = s.gethostbyname(my_hostname) 
 print('Your Ip Address is: ' + my_ip)
-host = 'hindimesabhijankari.com'
+host = 'gajendrasingh.tech'
 ip = s.gethostbyname(host)
 print('The IP Address of ' + host + ' is: '  + ip)
